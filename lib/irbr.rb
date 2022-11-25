5cb49b11798a7e0b629263134496647651f89984
Rafe Rosen <rafe@existentialmutt.com>
2022-11-24 20:42:48 -0500

    Initial commit

---
+++ lib/irbr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "irbr/version"
+
+module Irbr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/irbr/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Irbr
+  VERSION = "0.1.0"
+end
